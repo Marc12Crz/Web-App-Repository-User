6db0c96bb3d225c63fc0fd070947adedeb6c32f2
Adrian <adriannuno56@gmail.com>
2024-10-23 03:50:33 -0500

    Slidebar y Welcome

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,27 @@
-// src/components/Navbar.jsx
 import { Link } from 'react-router-dom';
-import '../HeaderFooterStyles.css'; // Importamos los estilos
-import Logo from '../assets/PawFriends_Logo.webp'; // Importamos el logo
-import ProfileIcon from '../assets/user.png'; // Importamos el ícono de perfil
+import Logo from '../assets/PawFriends_Logo.webp';  // Importar logo
+import ProfileIcon from '../assets/user.png';       // Importar icono de usuario
+import Slidebar from './Slidebar';                 // Importar el slidebar
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            {/* Navegación izquierda */}
-            <div className="navbar-left">
-                <ul>
-                    <li><Link to="/">Inicio</Link></li>
-                    <li><Link to="/adopciones">Adopciones</Link></li>
-                    <li><Link to="/donaciones">Donaciones</Link></li>
-                    <li><Link to="/albergues">Albergues</Link></li>
-                </ul>
-            </div>
-
-            {/* Logo en el centro */}
-            <div className="navbar-center">
-                <img src={Logo} alt="PawFriends Logo" className="navbar-logo" />
-            </div>
-
-            {/* Ícono de perfil a la derecha */}
-            <div className="navbar-right">
-                <img src={ProfileIcon} alt="Perfil" className="profile-icon" />
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="navbar">
+      {/* Slidebar deslizante */}
+      <Slidebar />
+      
+      {/* Logo centrado con enlace */}
+      <div className="navbar-center">
+        <Link to="/welcome">
+          <img src={Logo} alt="PawFriends Logo" />
+        </Link>
+      </div>
+      
+      {/* Icono de perfil a la derecha */}
+      <div className="navbar-right">
+        <img src={ProfileIcon} alt="Perfil" className="profile-icon" />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
