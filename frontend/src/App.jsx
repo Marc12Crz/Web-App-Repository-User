cac0a3f37a4b439737e8acdf0e97a46e8d378258
Adrian <adriannuno56@gmail.com>
2024-10-23 13:50:22 -0500

    Perfil y Menu User

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-// src/App.jsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './components/Login';
 import Register from './components/Register';
 import Welcome from './components/Welcome';
+import Perfil from './components/Perfil';  // Importamos el componente Perfil
 
 function App() {
     return (
@@ -16,6 +16,7 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/welcome" element={<Welcome />} />
+                    <Route path="/perfil" element={<Perfil />} />  {/* Ruta añadida para Perfil */}
                 </Routes>
             </div>
             <Footer />
